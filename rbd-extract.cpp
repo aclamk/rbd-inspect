c5f60588217a69c6ba2067446033edc18a6954cf
Adam Kupczyk <akupczyk@redhat.com>
2018-09-17 11:13:20 +0200

    Added help, added options.
    Signed-off-by: Adam Kupczyk <akupczyk@redhat.com>

--- rbd-extract.cpp
+++ rbd-extract.cpp
@@ -9,76 +9,136 @@
 #include <map>
 #include <vector>
 #include <set>
+#include <list>
 #include <assert.h>
 
 #include "parse_log.h"
 
-#include "model.h"
-
-int main(int argc, char** argv)
+int convert(std::istream& input, std::ostream& output)
 {
-  Model_t M;
-  std::map<uint32_t, Model_t> Models;
-  //std::map<uint32_t, Learn_t> Learns;
-  std::map<uint32_t, Recorder_t> Recorders;
-
-  std::ifstream rbd_log( argv[1] );
-  if (!rbd_log.is_open()) {
-    std::cerr << "Cannot open " << argv[1] << std::endl;
-  }
   std::string line;
+  typedef std::list<std::string> object_order_t;
+  typedef std::map<std::string, object_order_t::iterator> object_map_t;
+
+  object_order_t object_lru_order;
+  object_map_t object_lru;
   std::set<std::string> objects;
-  while (getline(rbd_log, line))
+
+  while (getline(input, line))
   {
     uint32_t client_no;
     std::string object_name;
     struct op_io_t specifics;
 
     if ( !parse_line(line, client_no, object_name, specifics) )
-	continue;
-    if (false) std::cout << std::fixed << std::setprecision(6) << specifics.tv <<
-	" " << specifics.client_no << " " << specifics.object_name << " " << specifics.opcode << " " << specifics.offset << " " << specifics.len << "." << std::endl;
+      continue;
 
-    //Learns[client_no].learn_object_op(Models[client_no], specifics);
-    Recorders[client_no].record(specifics);
-    //Models[client_no].r_w_print();
-  }
-  /*
-  for (auto &l: Learns) {
-    l.second.learn_end(Models[l.first]);
-  }
-  */
-
-  FILE *f = fopen("test-record", "a+");
-  for (auto &r: Recorders) {
-    //r.second.printall();
-    r.second.save(f);
-  }
-  fclose(f);
-  //print_history();
-
-
-  f = fopen("test-record", "r");
-
-  while (true) {
-    int ch = fgetc(f);
-    if (feof(f)) break;
-    ungetc(ch, f);
-    Player_t p;
-    assert(p.load(f));
-    Player_t::entry e;
-    while(p.pop_next(e)) {
-      //std::cout << "TIME=" << e.msec << " obj=" << e.obj_id << " " << e.operation << " o=" << e.offset << " l=" << e.len << std::endl;
+    if (objects.count(specifics.object_name) == 0) {
+      objects.emplace(specifics.object_name);
+      output << specifics.object_name << " add" << std::endl;
     }
+
+    if (object_lru.count(specifics.object_name) == 0) {
+      auto n = object_lru.emplace(specifics.object_name, object_lru_order.end());
+      assert(n.second);
+      object_lru_order.push_front(specifics.object_name);
+      n.first->second = object_lru_order.begin();
+      if (object_lru_order.size() > 1000) {
+        object_lru.erase(object_lru_order.back());
+        output << object_lru_order.back() << " close" << std::endl;
+        object_lru_order.pop_back();
+      }
+      output << specifics.object_name << " open" << std::endl;
+    } else {
+      object_lru_order.erase(object_lru[specifics.object_name]);
+      object_lru_order.push_front(specifics.object_name);
+      object_lru[specifics.object_name] = object_lru_order.begin();
+    }
+    output << specifics.object_name << " " << (specifics.opcode==op_write?"write":"read") <<
+        " " << specifics.offset << " " << specifics.len << std::endl;
   }
-  fclose(f);
-  /*
-  while (xxx-- > 0)
+  return 0;
+}
+
+
+
+void print_help()
+{
+  static const char help[] =
+      "Usage: rbd-extract [OPTION]...\n"
+      "Extract RBD operations from logs into fio's --read_iolog format.\n"
+      "\n"
+      " -i FILE         Read operations from rbd log FILE.\n"
+      " -o FILE         Write iolog output to FILE\n"
+      " -h, --help      Help\n";
+  std::cout << help << std::endl;
+}
+
+
+int main(int argc, char** argv)
+{
+  std::string input_name = "";
+  std::string output_name = "";
+
+  argc--; argv++;
+  std::set<std::string> oneparam_args={"-i", "-o"};
+  while (argc >= 1)
   {
-    if (M.r_w_model() == op_read)
-      std::cout << "R" << std::endl;
-    else
-      std::cout << "W" << std::endl;
+    std::string arg = *argv;
+    argc--; argv++;
+
+    if (oneparam_args.count(arg) > 0) {
+      //one param argument
+      if (argc < 1) {
+        std::cerr << "Option " << arg << " requires parameter" << std::endl;
+        exit(-1);
+      }
+      std::string value = *argv;
+      if (arg == "-i") {
+        input_name = value;
+      }
+      if (arg == "-o") {
+        output_name = value;
+      }
+      argc--; argv++;
+      continue;
+    }
+    if (arg == "-h" || arg == "--help") {
+      print_help();
+      return 0;
+    }
+    std::cerr << "Unknown option '" << arg << "'" << std::endl;
+    print_help();
+    return 1;
   }
-  */
+
+  std::ifstream rbd_log;
+  std::istream* input;
+  if (input_name == "") {
+    input = &std::cin;
+  } else {
+    rbd_log.open(input_name.c_str(), std::ifstream::binary);
+    if (!rbd_log.good()) {
+      std::cerr << "Cannot open '" << input_name << "' for writing" << std::endl;
+      return false;
+    }
+    input = &rbd_log;
+  }
+
+  std::ofstream foutput;
+  std::ostream* output;
+  if (output_name == "") {
+    output = &std::cout;
+  } else {
+    foutput.open(output_name.c_str(), std::ofstream::binary);
+    if (!foutput.good()) {
+      std::cerr << "Cannot open '" << output_name << "' for writing" << std::endl;
+      return false;
+    }
+    output = &foutput;
+  }
+
+  int result;
+  result = convert(*input, *output);
+  return result;
 }
